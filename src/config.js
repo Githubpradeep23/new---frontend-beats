cb3e9b88221896b09177f1652e0be16c1d4f77ba
Githubpradeep23 <123489512+Githubpradeep23@users.noreply.github.com>
2024-03-19 00:44:26 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-    baseURL: "https://beatsbackend.onrender.com", // replace with your actual base URL
+    baseURL: "http://ec2-35-154-153-93.ap-south-1.compute.amazonaws.com:8080", // replace with your actual base URL
     apiEndpoint: "/api/v1",
   };
   
